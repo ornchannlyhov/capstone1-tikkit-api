e10f34231e8915bd8ed8b7bb6aa857092ae89eee
lyhov <ornchannlyhov@gmail.com>
2025-02-13 19:51:59 +0700

    fix activity log bug

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -19,30 +19,39 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): JsonResponse  
+    public function store(Request $request): JsonResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'phone_number' => ['nullable', 'string', 'max:20'],
-        ]);
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users,email'],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+                'phone_number' => ['nullable', 'string', 'max:20'],
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation error',
+                'errors' => $e->errors(),
+            ], 422);
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password), 
+            'password' => Hash::make($request->password),
             'phone_number' => $request->phone_number,
         ]);
 
         event(new Registered($user));
-        ActivityLogHelper::logActivity($user, 'Register','User registered');
+        ActivityLogHelper::logActivity($user, 'Register', 'User registered');
         Auth::login($user);
         $token = $user->createToken('auth_token')->plainTextToken;
+
         return response()->json([
             'message' => 'Registration successful',
             'user' => $user,
             'token' => $token,
         ], 201);
     }
+
 }
