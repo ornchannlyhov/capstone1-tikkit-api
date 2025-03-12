d9a92dac441114ed195d7195986e51f17d730a65
lyhov <ornchannlyhov@gmail.com>
2025-03-12 14:07:43 +0700

    fix cartcontroller route issue

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\API\CartController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PurchasedTicketController;
